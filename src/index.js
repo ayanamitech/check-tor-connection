728a15d80be19b00117ba5834d346f28281ebf28
Ayanami <ayanami0330@protonmail.com>
2022-05-24 10:41:20 +0900

    Improve check-tor service
    + Improve logging format
    + Reduce default health check interval from 30 minutes to 5 minutes
    + Add support for more HEALTH_CHECK endpoint so the health check wouldn't perform tor restart for single endpoint failure

--- src/index.js
+++ src/index.js
@@ -8,10 +8,8 @@ const defaultConfig = {
   'RETRY_INTERVAL': 60,
   'RESTART_CMD': 'sudo systemctl restart tor',
   'HEALTH_CHECK': 'https://check.torproject.org/api/ip',
-  'HEALTH_CHECK_ONION': '',
-  'HEALTH_CHECK_INTERVAL': 1800,
+  'HEALTH_CHECK_INTERVAL': 300,
   'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
-  'USE_ONION': false,
   'TOR_HOST': '127.0.0.1',
   'TOR_PORT': 9050
 };
@@ -20,16 +18,27 @@ const config = loadConfig(defaultConfig);
 
 const setDelay = (secs = 1) => new Promise(resolve => setTimeout(() => resolve(), secs * 1000));
 
-const HEALTH_CHECK = (config.USE_ONION === true) && (config.HEALTH_CHECK_ONION) ? config.HEALTH_CHECK_ONION : config.HEALTH_CHECK;
-const isHTTP = (new URL(HEALTH_CHECK).protocol.split(':')[0] === 'http') ? true : false;
+const isHTTP = (host) => (new URL(host).protocol.split(':')[0] === 'http') ? true : false;
+
+const formatTime = () => {
+  const time = new Date();
+  const month = time.getMonth() + 1;
+  const day = time.getDate();
+  const year = time.getFullYear();
+  const rawHours = time.getHours();
+  const hours = (rawHours > 12) ? rawHours - 12 : rawHours;
+  const minutes = time.getMinutes();
+  const abbreviations = (rawHours > 12) ? 'PM' : 'AM';
+  return `${month}/${day}/${year} ${hours}:${minutes} ${abbreviations}`;
+};
 
 const restartTor = () => {
   try {
     const result = execSync(config.RESTART_CMD).toString();
-    console.log('Restarted Tor Network Service');
+    console.log(`${formatTime()} Restarted Tor Network Service`);
     console.log(`System log: ${result}`);
   } catch (e) {
-    console.error('Restarting Tor Network Service failed');
+    console.error(`${formatTime()} Restarting Tor Network Service failed`);
     console.error(`Error message: ${(e instanceof Error ? e.message : e)}`);
   }
 };
@@ -37,7 +46,7 @@ const restartTor = () => {
 /**
   Perform GET request to HEALTH_CHECK endpoint
 **/
-const healthCheck = async () => {
+const healthCheck = async (host) => {
   const axiosOptions = {
     headers: {
       'User-Agent': config.USER_AGENT
@@ -58,20 +67,20 @@ const healthCheck = async () => {
   while (retry < config.RETRY_TEST) {
     try {
       // Handle proxy agent for onion addresses
-      if (isHTTP) {
+      if (isHTTP(host)) {
         axiosOptions.httpAgent = new SocksProxyAgent(socksOptions);
       } else {
         axiosOptions.httpsAgent = new SocksProxyAgent(socksOptions);
       }
 
-      const data = await axios.get(HEALTH_CHECK, axiosOptions);
+      const data = await axios.get(host, axiosOptions);
       if (data.statusText === 'error' || data.data === undefined) {
-        throw new Error(`Error returned from health check point ${HEALTH_CHECK}, Error: ${data.status}`);
+        throw new Error(`Error returned from health check point ${host}, Error: ${data.status}`);
       }
-      console.log(`Health check success, server returned ${data.status}`);
+      console.log(`${formatTime()} Health check success for ${host}, server returned ${data.status}`);
       return true;
     } catch (e) {
-      console.error('Failed to perform health check, retrying');
+      console.error(`${formatTime()} Failed to perform health check for ${host}, retrying`);
       console.error(`Error message: ${(e instanceof Error ? e.message : e)}`);
       retry++;
     }
@@ -82,8 +91,9 @@ const healthCheck = async () => {
 
 const checkTor = () => {
   const startHealthCheck = async () => {
-    const result = await healthCheck();
-    if (result === false) {
+    const result = await Promise.all(config.HEALTH_CHECK.split(',').map(h => healthCheck(h)));
+    if (result.filter(r => r === false).length === result.length) {
+      console.error(`${formatTime()} All tests failed`);
       restartTor();
     }
   };
